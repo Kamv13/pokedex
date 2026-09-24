354e8193f717e19e322d4cd1c9d23d69f6338e11
Kevin Mendoza <ksmv233@gmail.com>
2026-09-24 14:32:58 -0600

    componentes y servicio creado

---
+++ src/app/components/type-badge/type-badge.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-type-badge',
+  imports: [],
+  templateUrl: './type-badge.html',
+  styleUrl: './type-badge.css',
+})
+export class TypeBadge {}
